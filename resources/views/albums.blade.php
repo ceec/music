6dfd51b18ef3a5e1cc101ec68279f49f70f397d5
Christine Stone <cc@battab.com>
2016-12-17 18:27:58 -0700

    Add form class

--- resources/views/albums.blade.php
+++ resources/views/albums.blade.php
@@ -5,9 +5,12 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
         <h1>All Albums</h1>
+
             @foreach($albums as $album)
         		{{$album->name}}<br>
         	@endforeach
+
+        	{!! Form::open(['url' => '/edit/card']) !!}     
         </div>
     </div>
 </div>
