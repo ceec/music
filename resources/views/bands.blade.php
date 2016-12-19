5cdd06a83aa59776d6d07f37c14405cae7b2162d
Christine Stone <cc@battab.com>
2016-12-19 16:33:14 -0700

    add tablesorter

--- resources/views/bands.blade.php
+++ resources/views/bands.blade.php
@@ -5,10 +5,44 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
         <h1>All Bands</h1>
-        	@foreach($bands as $band)
-        		{{$band->name}}<br>
-        	@endforeach
+        	<table id="all-bands" class="table">
+        		<thead>
+        			<th>Band</th>
+        			<th>Start Date</th>
+        			<th>Wesbite</th>
+        			<th>Active</th>
+        			<th></th>
+        			<th></th>
+        		</thead>
+        		<tbody>
+	        	@foreach($bands as $band)
+	        		<tr>
+	        			<td>{{$band->name}}</td>
+	        			<td>{{$band->start_date}}</td>
+	        			<td>{{$band->website}}</td>
+	        			<td>
+	        				@if ($band->still_active == 1)
+	        					Yes
+	        				@else
+	        					No
+	        				@endif
+	        			</td>
+	        			<td><button class="btn btn-primary">Edit</button></td>
+	        			<td><button class="btn btn-danger">Delete</button></td>
+	        		</tr>
+	        	@endforeach    
+	        	</tbody>    		
+        	</table>
+
         </div>
     </div>
 </div>
+
+<script type="text/javascript" src="/js/jquery-2.2.4.min.js"></script>
+<script type="text/javascript" src="/js/jquery.tablesorter.js"></script>
+<script>
+$(function(){
+  $("#all-bands").tablesorter();
+});
+</script>
 @endsection
